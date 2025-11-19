b8c495ac6c073f01c87faefb560dec5d6d75d675
Ricardo Millán <ricardo.millan139@gmail.com>
2025-11-19 14:12:39 -0600

    feat: me emocione y olvide hacer más commits, pero en este puedo decir que las funciones basicas ya se encuentran funcionando, implemente parseFloat, y realice pruebas finales con console.log mostrando resultaos correctos

---
+++ script.js
@@ -0,0 +1,49 @@
+// Las siguientes variables se definen en el ámbito global
+let tipoDeOperacion = parseInt(prompt("Selecciona el tipo de operación:\n1: Suma,\n2: Resta,\n3: Multiplicación,\n4: División\nSolo coloca el número de la opción deseada"));
+let num1 = parseFloat(prompt ("Ingresa el dato 1"));
+let num2 = parseFloat(prompt ("Ingresa el dato 2"));
+
+// Funciones establecidas
+function multiplicacion() {
+  return num1 * num2;
+}
+
+function suma() {
+  return num1 + num2;
+}
+
+function resta() {
+  return num1 - num2;
+}
+
+function divicion() {
+  return num1 / num2;
+}
+
+// Variable para almacenar el resultado final
+let resultado; 
+
+switch (tipoDeOperacion) {
+    case 1:
+        resultado = suma(); 
+        break;
+        
+    case 2:
+        resultado = resta();
+        break;
+        
+    case 3:
+        resultado = multiplicacion();
+        break;
+        
+    case 4:
+        resultado = divicion(); 
+        break;
+        
+    default:
+        // Maneja el caso en que el usuario no ingresó 1, 2, 3 o 4
+        resultado = "Error: Opción de operación no válida.";
+}
+
+// Muestra el resultado (o el mensaje de error) al usuario
+console.log("El total es: " + resultado);
